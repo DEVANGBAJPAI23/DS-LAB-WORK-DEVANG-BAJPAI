830b5b7ea7fc768c8f57dab588d4811aaaee4fd3
DEVANGBAJPAI23 <144374915+DEVANGBAJPAI23@users.noreply.github.com>
2023-10-28 14:30:48 +0530

    Update 40 Next greater to right, next greater to left, nearest smaller to left, next smaller to right.cpp

--- 40 Next greater to right, next greater to left, nearest smaller to left, next smaller to right.cpp
+++ 40 Next greater to right, next greater to left, nearest smaller to left, next smaller to right.cpp
@@ -1,46 +1,89 @@
-//NSE ,NGE
-vector<int> nge(vector<int> &arr){
-     int n=arr.size();
-    vector<int> output(n,-1);
-    stack<int> st;
-    indexes
-    st.push(0);
-    for (int i = 0; i < n; i++)
-    {
-    while(!st.empty()&& arr[i]>arr[st.top()])
-    {
-    output[st.top()]=arr[i];
-    st.pop();
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> nextGreaterToRight(const vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);
+    stack<int> s;
+    
+    for (int i = 0; i < n; ++i) {
+        while (!s.empty() && arr[i] > arr[s.top()]) {
+            result[s.top()] = arr[i];
+            s.pop();
+        }
+        s.push(i);
     }
-    st.push(); 
-    }
-        //for safety check
-    while(not st.top()){
-    output[st.top()]=-1;
-    st.pop();
-    }
-    return output; 
+    
+    return result;
 }
-//next smaller element
-vector<int> nse(vector<int> &arr){
-    int n=arr.size();
-    vector<int> output(n,-1);
-    stack<int> st;
-    indexes
-    st.push(0);
-    for (int i = 0; i < n; i++)
-    {
-    while(!st.empty()&& arr[i]<arr[st.top()])
-    {
-    output[st.top()]=arr[i];
-    st.pop();
+
+vector<int> nextGreaterToLeft(const vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);
+    stack<int> s;
+    
+    for (int i = n - 1; i >= 0; --i) {
+        while (!s.empty() && arr[i] > arr[s.top()]) {
+            result[s.top()] = arr[i];
+            s.pop();
+        }
+        s.push(i);
     }
-    st.push(); 
+    
+    return result;
+}
+
+vector<int> nearestSmallerToLeft(const vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);
+    stack<int> s;
+    
+    for (int i = 0; i < n; ++i) {
+        while (!s.empty() && arr[i] <= arr[s.top()]) {
+            s.pop();
+        }
+        if (!s.empty()) {
+            result[i] = arr[s.top()];
+        }
+        s.push(i);
     }
-        //for safety check
-    while(not st.top()){
-    output[st.top()]=-1;
-    st.pop();
+    
+    return result;
+}
+
+vector<int> nextSmallerToRight(const vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);
+    stack<int> s;
+    
+    for (int i = n - 1; i >= 0; --i) {
+        while (!s.empty() && arr[i] <= arr[s.top()]) {
+            s.pop();
+        }
+        if (!s.empty()) {
+            result[i] = arr[s.top()];
+        }
+        s.push(i);
     }
-    return output; 
+    
+    return result;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 2, 10, 8};
+    
+    vector<int> ngr = nextGreaterToRight(arr);
+    vector<int> ngl = nextGreaterToLeft(arr);
+    vector<int> nsl = nearestSmallerToLeft(arr);
+    vector<int> nsr = nextSmallerToRight(arr);
+    
+    // Printing the results
+    for (int i = 0; i < arr.size(); ++i) {
+        cout << "Element " << arr[i] << ": NGR=" << ngr[i] << ", NGL=" << ngl[i] << ", NSL=" << nsl[i] << ", NSR=" << nsr[i] << endl;
+    }
+    
+    return 0;
 }
