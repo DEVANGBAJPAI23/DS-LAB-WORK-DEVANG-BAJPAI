80dc3d3c002064c210abc184f9270143df4a1d71
DEVANGBAJPAI23 <144374915+DEVANGBAJPAI23@users.noreply.github.com>
2023-11-01 10:50:18 +0530

    Update 42 create a binary tree.cpp

--- 42 create a binary tree.cpp
+++ 42 create a binary tree.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
 class node{
@@ -34,7 +34,7 @@ void levelorder(node* root){
     queue<node*> q;
     q.push(root);
     q.push(NULL);
-    while(!q.empty()){j
+    while(!q.empty()){
         node*temp = q.front();
         q.pop();
         if(temp == NULL){
